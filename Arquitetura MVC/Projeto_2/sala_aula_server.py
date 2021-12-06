547c09a89712b61700f38cf3d61910942e77a21a
Patricia Magalhães <paath.magalhaes2@gmail.com>
2021-12-05 23:25:57 -0300

    utilizando padrão MVC

---
+++ Arquitetura MVC/Projeto_2/sala_aula_server.py
@@ -0,0 +1,35 @@
+from flask import Flask, jsonify, request
+
+import controller.aluno_controller as aluno_controller
+
+app = Flask(__name__)
+
+@app.route("/alunos")
+def getAlunos():
+    return aluno_controller.listar()
+
+@app.route("/alunos/<int:id_consulta>", methods=["GET"])
+def getAlunoId(id_consulta):
+    return aluno_controller.localizaPorId(id_consulta)
+
+@app.route("alunos/maior_media", methods=["GET"])
+def getAlunoMaiorMedia():
+    return aluno_controller.localizarPorMaiorMedia()
+
+@app.route("/alunos", methods=["POST"])
+def inserir():
+    aluno = request.json
+    return aluno_controller.inserirAluno(aluno)
+
+@app.route("/alunos/<int:id_deletar>", methods=["DELETE"])
+def excluir(id_deletar):
+    aluno_controller.excluirPorId(id_deletar)
+
+@app.route("/alunos/<int:id_alterar>", methods=["PUT"])
+def alterar(id_alterar):
+    aluno = request.json
+    aluno_controller.alterarAluno(id_alterar, aluno)
+    
+if __name__ == "__main__":
+    app.run(host="localhost", port=5002)
+    
